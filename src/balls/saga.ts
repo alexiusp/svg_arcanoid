fc73a80cc4687905b4f0848319a50cf7bfe03301
Aleksei AlexiuS Podgaev <podgaev@gmail.com>
2020-03-01 01:19:38 +0100

    ball-brick collision detection introduced

--- src/balls/saga.ts
+++ src/balls/saga.ts
@@ -1,5 +1,8 @@
-import { all, call, put, select, takeLatest, takeEvery } from 'redux-saga/effects';
+import { all, put, select, takeLatest, takeEvery } from 'redux-saga/effects';
 
+import { ICaretModel } from '../caret/types';
+import { IRect } from '../types';
+import { Selectors as BricksSelectors } from '../bricks';
 import { Selectors as CaretSelectors } from '../caret';
 import { VIEW_WIDTH, VIEW_HEIGHT } from '../constants';
 import { Actions as AppActions } from '../app';
@@ -7,7 +10,6 @@ import * as ActionTypes from './actionTypes';
 import * as Actions from './actions';
 import * as Selectors from './selectors';
 import { IBallModel } from './types';
-import { ICaretModel } from '../caret/types';
 
 const DEFAULT_BALL_RADIUS = 12;
 // real coordinates ball speed multiplier
@@ -34,40 +36,60 @@ function lengthSqr(x1: number, y1: number, x2: number, y2: number) {
   return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
 }
 
-function* detectCollision(x: number, y: number, r: number) {
-  // detect if ball collide with caret
-  const caret: ICaretModel = yield select(CaretSelectors.getCaretModel);
-  const isLeft = x <= caret.x;
-  const isRight = x >= caret.x + caret.width;
-  const isTop = y <= caret.y;
-  const isBottom = y >= caret.y + caret.height;
+enum ERectCollision {
+  None,
+  LeftTop,
+  Left,
+  LeftBottom,
+  Bottom,
+  RightBottom,
+  Right,
+  RightTop,
+  Top,
+  Inside,
+}
+
+function getCollision(ball: IBallModel, rect: IRect) {
+  const isLeft = ball.x <= rect.x;
+  const isRight = ball.x >= rect.x + rect.width;
+  const isTop = ball.y <= rect.y;
+  const isBottom = ball.y >= rect.y + rect.height;
+  let collide = false;
   if (isLeft && isTop) {
     // left-top corner
-    return lengthSqr(x, y, caret.x, caret.y) <= Math.pow(r, 2);
+    collide = lengthSqr(ball.x, ball.y, rect.x, rect.y) <= Math.pow(ball.r, 2);
+    return collide ? ERectCollision.LeftTop : ERectCollision.None;
   } else if (isLeft && isBottom) {
     // left-bottom corner
-    return lengthSqr(x, y, caret.x, caret.y + caret.height) <= Math.pow(r, 2);
+    collide = lengthSqr(ball.x, ball.y, rect.x, rect.y + rect.height) <= Math.pow(ball.r, 2);
+    return collide ? ERectCollision.LeftBottom : ERectCollision.None;
   } else if (isLeft) {
     // left side
-    return caret.x - x <= r;
+    collide = rect.x - ball.x <= ball.r;
+    return collide ? ERectCollision.Left : ERectCollision.None;
   } else if (isRight && isBottom) {
     // right-bottom corner
-    return lengthSqr(x, y, caret.x + caret.width, caret.y + caret.height) <= Math.pow(r, 2);
+    collide = lengthSqr(ball.x, ball.y, rect.x + rect.width, rect.y + rect.height) <= Math.pow(ball.r, 2);
+    return collide ? ERectCollision.RightBottom : ERectCollision.None;
   } else if (isBottom) {
     // bottom side
-    return y - caret.y + caret.height <= r;
+    collide = ball.y - (rect.y + rect.height) <= ball.r;
+    return collide ? ERectCollision.Bottom : ERectCollision.None;
   } else if (isRight && isTop) {
     // right-top corner
-    return lengthSqr(x, y, caret.x + caret.width, caret.y) <= Math.pow(r, 2);
+    collide = lengthSqr(ball.x, ball.y, rect.x + rect.width, rect.y) <= Math.pow(ball.r, 2);
+    return collide ? ERectCollision.RightTop : ERectCollision.None;
   } else if (isRight) {
     // right side
-    return x - caret.x + caret.width <= r;
+    collide = ball.x - (rect.x + rect.width) <= ball.r;
+    return collide ? ERectCollision.Right : ERectCollision.None;
   } else if (isTop) {
     // top side
-    return caret.y - y <= r;
+    collide = rect.y - ball.y <= ball.r;
+    return collide ? ERectCollision.Top : ERectCollision.None;
   }
   // inside
-  return true;
+  return ERectCollision.Inside;
 }
 
 function* updateBallsSaga() {
@@ -79,38 +101,6 @@ function* updateBallsSaga() {
     let posY = ball.y + ball.vy * SPEED_STEP;
     let newVX = ball.vx;
     let newVY = ball.vy;
-    // TODO: resolve objects collisions
-    const collideWithCaret = yield call(detectCollision, posX, posY, ball.r);
-    if (collideWithCaret) {
-      const caret: ICaretModel = yield select(CaretSelectors.getCaretModel);
-      // does not matter wich side of caret collide
-      newVY = -newVY;
-      newVX += caret.speed * IMPULSE_RATIO;
-    } else {
-      // resolve edge collisions
-      // Detect collision with floor.
-      if (posY + ball.r > VIEW_HEIGHT) {
-        newVX = 0;
-        newVY = 0;
-        yield put(Actions.ballHitFloorAction(index));
-      }
-      // Detect collision with right wall.
-      else if (posX + ball.r > VIEW_WIDTH) {
-        // Need to know how much we overshot the canvas width so we know how far to 'bounce'.
-        posX = VIEW_WIDTH - ball.r;
-        newVX = -newVX;
-      }
-      // Detect collision with left wall.
-      else if (posX - ball.r < 0) {
-        posX = ball.r;
-        newVX = -newVX;
-      }
-      // Detect collision with top wall.
-      else if (posY - ball.r < 0) {
-        posY = ball.r;
-        newVY = -newVY;
-      }
-    }
     const newBall: IBallModel = {
       ...ball,
       x: posX,
@@ -118,6 +108,67 @@ function* updateBallsSaga() {
       vx: newVX,
       vy: newVY,
     };
+    // check for collision with bricks
+    // TODO: optimise check by reducing set of bricks under test
+    const bricks = yield select(BricksSelectors.getBricks);
+    let brickCollision = ERectCollision.None;
+    for (let brickIndex = 0; brickIndex < bricks.length; brickIndex++) {
+      const brick = bricks[brickIndex];
+      brickCollision = getCollision(newBall, brick);
+      if (brickCollision !== ERectCollision.None) {
+        // collision found - exit loop
+        break;
+      }
+    }
+    switch (brickCollision) {
+      case ERectCollision.Bottom:
+      case ERectCollision.Top:
+        newBall.vy = -newBall.vy;
+        break;
+      case ERectCollision.Right:
+      case ERectCollision.Left:
+        newBall.vx = -newBall.vx;
+        break;
+      case ERectCollision.LeftBottom:
+      case ERectCollision.LeftTop:
+      case ERectCollision.RightBottom:
+      case ERectCollision.RightTop:
+      case ERectCollision.Inside:
+        newBall.vy = -newBall.vy;
+        newBall.vx = -newBall.vx;
+    }
+    // check for collision with caret
+    const caret: ICaretModel = yield select(CaretSelectors.getCaretModel);
+    const caretCollision = getCollision(newBall, caret);
+    if (caretCollision !== ERectCollision.None) {
+      // does not matter wich side of caret collide
+      newBall.vy = -newBall.vy;
+      newBall.vx += caret.speed * IMPULSE_RATIO;
+    } else {
+      // resolve collisions with edge of the viewport
+      // Detect collision with floor.
+      if (newBall.y + ball.r > VIEW_HEIGHT) {
+        newBall.vx = 0;
+        newBall.vy = 0;
+        yield put(Actions.ballHitFloorAction(index));
+      }
+      // Detect collision with right wall.
+      else if (newBall.x + ball.r > VIEW_WIDTH) {
+        // Need to know how much we overshot the canvas width so we know how far to 'bounce'.
+        newBall.x = VIEW_WIDTH - ball.r;
+        newBall.vx = -newBall.vx;
+      }
+      // Detect collision with left wall.
+      else if (newBall.x - ball.r < 0) {
+        newBall.x = ball.r;
+        newBall.vx = -newBall.vx;
+      }
+      // Detect collision with top wall.
+      else if (newBall.y - ball.r < 0) {
+        newBall.y = ball.r;
+        newBall.vy = -newBall.vy;
+      }
+    }
     yield put(Actions.updateBallAction(index, newBall));
   }
 }
